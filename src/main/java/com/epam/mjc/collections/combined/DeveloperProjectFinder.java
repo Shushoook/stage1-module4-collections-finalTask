83c1e5fc1c331abe23a2439c8cc76c59f4efeace
Shushoook <shushoook666@gmail.com>
2023-02-26 20:43:17 +0400

    stage1-module4-collections-finalTask

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,27 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+        for (String projectName : projects.keySet()) {
+            Set<String> developers = projects.get(projectName);
+            if (developers.contains(developer)) {
+                result.add(projectName);
+            }
+        }
+        Collections.sort(result, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                int lenComp = Integer.compare(s2.length(), s1.length());
+                if (lenComp != 0) {
+                    return lenComp;
+                } else {
+                    return s2.compareTo(s1);
+                }
+            }
+        });
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,19 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> result = new HashMap<>();
+        for (String key : sourceMap.keySet()) {
+            int length = key.length();
+            Set<String> keysWithLength = result.getOrDefault(length, new HashSet<>());
+            keysWithLength.add(key);
+            result.put(length, keysWithLength);
+        }
+        return result;
     }
 }
